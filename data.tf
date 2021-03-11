015f4a6f3b103afe7556cf039e66c36bc06d7ed2
shin1230 <shin1230@gmail.com>
2021-03-11 19:03:53 +0900

    comment data

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-data "aws_subnet_ids" "EDUID"" {
-  vpc_id = var.vpc_id
-}
+# data "aws_subnet_ids" "EDUID"" {
+#   vpc_id = var.vpc_id
+# }
 
-data "aws_subnet" "EDU" {
-  for_each = data.aws_subnet_ids.EDUID.ids
-  id       = each.value
-}
+# data "aws_subnet" "EDU" {
+#   for_each = data.aws_subnet_ids.EDUID.ids
+#   id       = each.value
+# }
