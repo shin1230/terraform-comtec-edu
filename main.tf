a6c711ea27804040c3f2b95edb657d0014ec55f8
shin1230 <shin1230@gmail.com>
2021-03-11 18:47:48 +0900

    add subnet_id

--- main.tf
+++ main.tf
@@ -88,11 +88,12 @@ resource "aws_security_group" "EDU-SG" {
 resource "aws_instance" "EC2-EDU" {
   ami = "ami-0e17ad9abf7e5c818"
   instance_type = "t2.micro"
+  subnet_id = aws_vpc.EDU-VPC.subnet_id
 
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
+  # network_interface {
+  #   network_interface_id = aws_network_interface.foo.id
+  #   device_index         = 0
+  # }
 
   security_groups = [aws_security_group.EDU-SG.id]
 
