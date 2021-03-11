039da9fd2fcce0814da808f4d3240620e5b1121d
shin1230 <shin1230@gmail.com>
2021-03-11 14:54:11 +0900

    create ec2, vpc, subnet

--- main.tf
+++ main.tf
@@ -12,3 +12,43 @@ provider "aws" {
   # region  = "ap-northeast-2"
 }
 
+resource "aws_vpc" "EDU-VPC" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name    = "EDU-VPC"
+  }
+
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.EDU-VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.public-subnet.id
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "EC2-EDU" {
+  ami = "ami-05b616430d239765b"
+  instance_type = "t3.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "EC2-EDU"
+  }
+}
