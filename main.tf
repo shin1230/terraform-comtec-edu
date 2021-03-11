6d4e6631acf6d56ffffaa27348eb85c600c99b59
shin1230 <shin1230@gmail.com>
2021-03-11 18:57:21 +0900

    add data, var

--- main.tf
+++ main.tf
@@ -12,18 +12,18 @@ provider "aws" {
   region  = "ap-northeast-2"
 }
 
-resource "aws_vpc" "EDU-VPC" {
+resource "aws_vpc" "${var.prefix}-VPC" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
-    Name    = "EDU-VPC"
+    Name    = "${var.prefix}-VPC"
   }
 
 }
 
 resource "aws_subnet" "public-subnet" {
-  vpc_id = aws_vpc.EDU-VPC.id
+  vpc_id = aws_vpc.${var.prefix}-VPC.id
   cidr_block = "10.0.1.0/24"
   availability_zone       = "ap-northeast-2a"
 
@@ -40,31 +40,44 @@ resource "aws_network_interface" "foo" {
   }
 }
 
-resource "aws_internet_gateway" "EDU-IGW" {
-  vpc_id = aws_vpc.EDU-VPC.id
+resource "aws_internet_gateway" "${var.prefix}-IGW" {
+  vpc_id = aws_vpc.${var.prefix}-VPC.id
 
   tags = {
-    Name    = "EDU-IGW"
+    Name    = "${var.prefix}-IGW"
   }
 }
 
 resource "aws_route_table" "Public-Route" {
-  vpc_id = aws_vpc.EDU-VPC.id
+  vpc_id = aws_vpc.${var.prefix}-VPC.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.EDU-IGW.id
+    gateway_id = aws_internet_gateway.${var.prefix}-IGW.id
   }
 
   tags = {
-    Name    = "EDU-Public-Route"
+    Name    = "${var.prefix}-Public-Route"
   }
 }
 
-resource "aws_security_group" "EDU-SG" {
-  name = "EDU-security-group"
 
-  vpc_id = aws_vpc.EDU-VPC.id
+resource "aws_subnet" "Public-A" {
+  vpc_id                  = aws_vpc.PROD-VPC.id
+  cidr_block              = var.Subnet-A_prefix
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.prefix}-Public-Subnet-A"
+    Service = var.prefix
+  }
+}
+
+resource "aws_security_group" "${var.prefix}-SG" {
+  name = "${var.prefix}-security-group"
+
+  vpc_id = aws_vpc.${var.prefix}-VPC.id
 
   ingress {
     from_port   = 22
@@ -81,24 +94,24 @@ resource "aws_security_group" "EDU-SG" {
   }
 
   tags = {
-    Name    = "EDU-SG"
+    Name    = "${var.prefix}-SG"
   }
 }
 
-resource "aws_instance" "EC2-EDU" {
+resource "aws_instance" "EC2-${var.prefix}" {
   ami = "ami-0e17ad9abf7e5c818"
   instance_type = "t2.micro"
-  subnet_id = aws_vpc.EDU-VPC.subnet_id
+  subnet_id = aws_vpc.${var.prefix}-VPC.subnet_id
 
   # network_interface {
   #   network_interface_id = aws_network_interface.foo.id
   #   device_index         = 0
   # }
 
-  security_groups = [aws_security_group.EDU-SG.id]
+  security_groups = [aws_security_group.${var.prefix}-SG.id]
 
   tags = {
-    Name = "EC2-EDU"
+    Name = "EC2-${var.prefix}"
   }
 }
 
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "subnet_cidr_blocks" {
+  value = [for s in data.aws_subnet.EDU : s.cidr_block]
+}
